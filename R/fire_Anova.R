6a7194063e04ad7b76a72fa1b4e2f109ea2debf6
jwkelley <jaykelley88@gmail.com>
2019-10-12 23:42:16 -0700

    Implimenting the first for loop i for agencies.

--- R/fire_Anova.R
+++ R/fire_Anova.R
@@ -15,88 +15,98 @@
 #' @import gridExtra
 
 
-library(dplyr)
-library(tidyr)
-library(ggpubr)
-library(gdata)
-library(ggplot2)
-library(reshape2)
-library(grid)
-library(gridExtra)
-library(cowplot)
+fire_Anova <- function(fires, agency){
 
-setwd("D:/R_Package_Cbone_FIRES/Code/TestPackage/")
+  require(dplyr)
+  require(tidyr)
+  require(ggpubr)
+  require(gdata)
+  require(ggplot2)
+  require(reshape2)
+  require(grid)
+  require(gridExtra)
+  require(cowplot)
 
-Fires <- read.csv("Fires_AllCalcs.csv")
+  #####
+  ##Should the following variables be passed to the function and be dynamic?
+  ##
+  var.name <- c("PM2.5","PM10","Lead","CO","NO2","O3","SO2","wind", "tavg", "tmax", "tmin", "precip","PSDI")
+  state.abbr <- c("AZ","CA","CO","ID","MT","NV","NM","OR","UT","WA","WY")
+  state.num <- c(4,6,8,16,30,32,35,41,49,53,56)
+  state.name <- c("Arizona", "California","Colorado","Idaho","Montana","Nevada","New Mexico","Oregon","Utah","Washington","Wyoming")
 
+  ### divide Fires into USFS & DOI
 
-var.name <- c("PM2.5","PM10","Lead","CO","NO2","O3","SO2","wind", "tavg", "tmax", "tmin", "precip","PSDI")
-state.abbr <- c("AZ","CA","CO","ID","MT","NV","NM","OR","UT","WA","WY")
-state.num <- c(4,6,8,16,30,32,35,41,49,53,56)
-state.name <- c("Arizona", "California","Colorado","Idaho","Montana","Nevada","New Mexico","Oregon","Utah","Washington","Wyoming")
+  for(i in 1:length(agency)){
+    i = 1
+    DOI <- fires[fires$Source=="DOI",]
+    USFS <- fires[fires$Source=="USFS",]
 
-### divide Fires into USFS & DOI
-DOI <- Fires[Fires$Source=="DOI",]
-USFS <- Fires[Fires$Source=="USFS",]
-plots <- list()
-####################################################################
-data.source <- DOI            
-source <- data.source[1,"Source"]
-variable <- data.source$tavg 
-                 
-##################################################################
-aov <- aov(variable ~ State, data=data.source) 
-tukey <- TukeyHSD(aov)
-file <- as.data.frame(tukey$State)
-file <- subset(file, select = c("p adj"))
-file$States <- rownames(file)
-file$`p adj` <- round(file$`p adj`,2)
-file <- as.data.frame(file) %>% separate(States, into = c("State1","State2"))
-rownames(file) <- c(seq(1:55))
-file <- file[c(2,3,1)]
+    agentFire <- fires[as.character(fires$Source) == agency[[i]],]
 
+    plots_Weather <- list()
+    plots_Pollutants <- list()
 
-file$p <- cut(file$"p adj",breaks = c(0,0.01,0.05,0.10,1.001),right = FALSE)
+    ####################################################################
+    data.source <- DOI
+    source <- data.source[1,"Source"]
 
-plots[[5]] <- ggplot(file, aes(State1, State2)) + 
-  geom_tile(aes(fill = p), color="white") +
-  scale_y_discrete( position = "right") +
-  scale_fill_manual(labels=c("0.00-0.01","0.01-0.05", "0.05-0.10", "0.10-1.00"),
-                    values=c("#e31a1c","#fd8d3c","#fecc5c","#ffffb2")) +
-  labs(fill = "P Value") +
-  xlab(NULL) +
-  ylab(NULL) +
-  ggtitle("Average Temperature") +
-  theme(legend.position="none")
+    variable <- data.source$tavg
+
+    ##################################################################
+    aov <- aov(variable ~ State, data=data.source)
+    tukey <- TukeyHSD(aov)
+    file <- as.data.frame(tukey$State)
+    file <- subset(file, select = c("p adj"))
+    file$States <- rownames(file)
+    file$`p adj` <- round(file$`p adj`,2)
+    file <- as.data.frame(file) %>% separate(States, into = c("State1","State2"))
+    rownames(file) <- c(seq(1:55))
+    file <- file[c(2,3,1)]
 
 
-##### store legend ###################################################################
-b <- ggplot(file, aes(State1, State2)) + 
-          geom_tile(aes(fill = p), color="white") +
-          scale_y_discrete( position = "right") +
-          scale_fill_manual(labels=c("0.00-0.01","0.01-0.05", "0.05-0.10", "0.10-1.00"),
-            values=c("#e31a1c","#fd8d3c","#fecc5c","#ffffb2")) +
-         labs(fill = "P Value") +
-          xlab(NULL) +
-          ylab(NULL) 
+    file$p <- cut(file$"p adj",breaks = c(0,0.01,0.05,0.10,1.001),right = FALSE)
 
-extractLegend <- function(gg) {
-  grobs <- ggplot_gtable(ggplot_build(gg))
-  foo <- which(sapply(grobs$grobs, function(x) x$name) == "guide-box")
-  grobs$grobs[[foo]]
+    plots[[5]] <- ggplot(file, aes(State1, State2)) +
+      geom_tile(aes(fill = p), color="white") +
+      scale_y_discrete( position = "right") +
+      scale_fill_manual(labels=c("0.00-0.01","0.01-0.05", "0.05-0.10", "0.10-1.00"),
+                        values=c("#e31a1c","#fd8d3c","#fecc5c","#ffffb2")) +
+      labs(fill = "P Value") +
+      xlab(NULL) +
+      ylab(NULL) +
+      ggtitle("Average Temperature") +
+      theme(legend.position="none")
+
+
+    ##### store legend ###################################################################
+    b <- ggplot(file, aes(State1, State2)) +
+              geom_tile(aes(fill = p), color="white") +
+              scale_y_discrete( position = "right") +
+              scale_fill_manual(labels=c("0.00-0.01","0.01-0.05", "0.05-0.10", "0.10-1.00"),
+                values=c("#e31a1c","#fd8d3c","#fecc5c","#ffffb2")) +
+             labs(fill = "P Value") +
+              xlab(NULL) +
+              ylab(NULL)
+
+    extractLegend <- function(gg) {
+      grobs <- ggplot_gtable(ggplot_build(gg))
+      foo <- which(sapply(grobs$grobs, function(x) x$name) == "guide-box")
+      grobs$grobs[[foo]]
+    }
+    legend <- extractLegend(b)
+    plots[[6]] <- ggdraw(legend)
+    ############################################################################
+
+    # lay <- rbind(c(1,1,1,2,2,2,3,3,3,4,4,4),
+                 # c(5,5,5,6,6,6,7,7,7,NA,8,NA))
+    lay <- rbind(c(1,1,1,2,2,2,3,3,3),
+                 c(4,4,4,5,5,5,NA,6,NA))
+
+    # setwd("F:/Rx Fire/ANOVA/")
+    pdf("DOI_weather.pdf", width=10.5, height=7, onefile=FALSE)
+    figure <- grid.arrange(grobs=plots,layout_matrix=lay)
+    print(figure)
+    dev.off()
+  }
 }
-legend <- extractLegend(b)
-plots[[6]] <- ggdraw(legend)
-############################################################################
-
-# lay <- rbind(c(1,1,1,2,2,2,3,3,3,4,4,4),
-             # c(5,5,5,6,6,6,7,7,7,NA,8,NA))
-lay <- rbind(c(1,1,1,2,2,2,3,3,3),
-             c(4,4,4,5,5,5,NA,6,NA))
-
-# setwd("F:/Rx Fire/ANOVA/")
-pdf("DOI_weather.pdf", width=10.5, height=7, onefile=FALSE)
-figure <- grid.arrange(grobs=plots,layout_matrix=lay)
-print(figure)
-dev.off()
-
